dba4f932801a0bb4921773863fb2df9cddf88902
lazybrotherlazy <lazylinks69@gmail.com>
2026-07-11 20:33:24 +0530

    Postgres via DATABASE_URL, sqlite fallback for local

--- pawfinder/settings.py
+++ pawfinder/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,10 +76,9 @@ WSGI_APPLICATION = 'pawfinder.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3')
+    )
 }
 
 
